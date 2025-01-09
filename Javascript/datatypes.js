4bf57a5e60557973ed994861b11e5ba19ad532a8
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-01-09 15:01:49 +0530

    added js file

---
+++ Javascript/datatypes.js
@@ -0,0 +1,24 @@
+let name = "John Doe";
+console.log("Name: " + name);
+
+let age = 30;
+console.log("Age: " + age);
+
+let isStudent = true;
+console.log("Is student: " + isStudent);
+
+let undefinedVariable;
+console.log("Undefined variable: " + undefinedVariable);
+
+let nullVariable = null;
+console.log("Null variable: " + nullVariable);
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+console.log("Person: " + JSON.stringify(person));
+
+let colors = ["Red", "Green", "Blue"];
+console.log("Colors: " + colors.join(", "));
