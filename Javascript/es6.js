de0d53f73dc443c35c39b55838f7332f488665d6
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-01-10 11:57:33 +0530

    started ECMAscript

---
+++ Javascript/es6.js
@@ -0,0 +1,9 @@
+let name = 'John Doe';
+const age = 30;
+
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+
+console.log(greet(name));
+console.log(`Age: ${age}`);
---
+++ Javascript/looping-D5.js
@@ -0,0 +1,5 @@
+let c= [2,3,5,3,5]
+
+for(let i in c){
+    console.log(c[i]);
+}
